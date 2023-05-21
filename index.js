7d38f1dc8166f6bae10b62f9bdf1e9cfe5fc7f3c
Aleksandr Novychenko <A.Novychenko@outlook.com>
2023-05-21 15:48:32 +0300

    test

---
+++ index.js
@@ -0,0 +1,7 @@
+var add = function (num1, num2) {
+  return num1 + num2;
+};
+var a = 1;
+var b = 1;
+
+console.log("add", add(a, b));
---
+++ index.ts
@@ -0,0 +1,6 @@
+const add = (num1: number, num2: number) => num1 + num2;
+
+const a: number = 1;
+const b: number = 1;
+
+console.log("add", add(a, b));
