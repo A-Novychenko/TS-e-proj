3a61284fecc0a0727900e2604b003d1a2ad1d9ca
Aleksandr Novychenko <A.Novychenko@outlook.com>
2023-06-01 22:11:25 +0300

    lesson 5 - tasks done

--- src/app.ts
+++ src/app.ts
@@ -21,7 +21,10 @@ type AllType = {
 
 // Є функція, вона приймає два аргументи, в один потрапляє name і color, в іншу частину - position і weight. Потрібно явно вказати, що ці поля з AllType. І сама функція повертає AllType. Скористайтеся Pick.
 
-function compare(top, bottom): AllType {
+function compare(
+  top: Pick<AllType, "name" | "color">,
+  bottom: Pick<AllType, "position" | "weight">
+): AllType {
   return {
     name: top.name,
     color: top.color,
@@ -32,17 +35,21 @@ function compare(top, bottom): AllType {
 
 // Вкажіть дженерики для функції об'єднання об'єктів.
 
-function merge(objA, objB) {
+function merge<T extends object, U extends object>(objA: T, objB: U) {
   return Object.assign(objA, objB);
 }
 
 // У вас є наступні класи
 
-class Component {
+class Component<T> {
   constructor(public props: T) {}
 }
 
-class Page extends Component {
+interface IProps {
+  title: string;
+}
+
+class Page extends Component<IProps> {
   pageInfo() {
     console.log(this.props.title);
   }
